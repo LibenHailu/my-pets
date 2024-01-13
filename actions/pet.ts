913286d716d158ba4f8089f2b1b11d9ec33d7a6a
Liben Hailu <libenhailu04@gmail.com>
2024-01-13 16:37:19 +0300

    add delte pet functionality

--- actions/pet.ts
+++ actions/pet.ts
@@ -1,25 +1,23 @@
-"use server"
+"use server";
 import { revalidatePath } from "next/cache";
-import { sql } from '@vercel/postgres';
-import { z } from 'zod'
+import { sql } from "@vercel/postgres";
+import { z } from "zod";
 
 const schema = z.object({
-    name: z.string().min(1, { message: 'Pet name required.' }),
-    owner: z.string().min(1, { message: 'Owner name required.' })
-})
+    name: z.string().min(1, { message: "Pet name required." }),
+    owner: z.string().min(1, { message: "Owner name required." }),
+});
 export async function createPet(prevState: any, formData: FormData) {
-    "use server"
     try {
-
         const validatedFields = schema.safeParse({
             name: formData.get("name") as string,
             owner: formData.get("owner") as string,
-        })
+        });
 
         if (!validatedFields.success) {
             return {
                 errors: validatedFields.error.flatten().fieldErrors,
-            }
+            };
         }
 
         await sql`
@@ -31,5 +29,19 @@ export async function createPet(prevState: any, formData: FormData) {
     } catch (e) {
         console.error(e);
         return { message: "Failed to create pet" };
+    }
+}
+
+export async function deletePet(formData: FormData) {
+    try {
+        const name = (await formData.get("name")) as string;
+        const owner = (await formData.get("owner")) as string;
+        await sql`DELETE FROM pets
+        WHERE Name = ${name} AND Owner = ${owner};`;
+
+        revalidatePath("/");
+    } catch (e) {
+        console.error(e);
+        return { message: "Failed to delete pet" };
     }
 }
--- components/pet-item.tsx
+++ components/pet-item.tsx
@@ -1,19 +1,26 @@
+import { deletePet } from "@/actions/pet";
+
 type PetItemProps = {
-    name: string,
-    owner: string
-}
+    name: string;
+    owner: string;
+};
 export const PetItem: React.FC<PetItemProps> = ({ name, owner }) => {
-    return (<div className="card w-full bg-base-100 shadow-xl">
-        <div className="card-body">
-            <div className="flex items-center justify-between">
-                <div>
-                    <h2 className="card-title">{name}!</h2>
-                    <p>{owner}</p>
-                </div>
-                <div>
-                    <button className="btn btn-active">Delete</button>
+    return (
+        <form className="card w-full bg-base-100 shadow-xl">
+            <div className="card-body">
+                <div className="flex items-center justify-between">
+                    <div>
+                        <input className="card-title disabled" name="name" defaultValue={name} />
+                        <input className="hidden" defaultValue={owner} name="owner" />
+                        <p className="text-sm">{owner}</p>
+                    </div>
+                    <div>
+                        <button formAction={deletePet} className="btn btn-active">
+                            Delete
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>)
-}
+        </form>
+    );
+};
